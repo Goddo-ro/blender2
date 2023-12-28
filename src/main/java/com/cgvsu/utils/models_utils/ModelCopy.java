7d709e7a62c1862fcdd15bb37599a88ac5a8f9af
MiddleShamil <dmitriy_abdullaev@bk.ru>
2023-12-28 10:45:39 +0300

    refactor: remove ModelCopy issues

--- src/main/java/com/cgvsu/utils/models_utils/ModelCopy.java
+++ src/main/java/com/cgvsu/utils/models_utils/ModelCopy.java
@@ -12,27 +12,24 @@ import java.util.Collections;
 
 public class ModelCopy {
     public static Model copyModel(Model model) {
-        // TODO: 26.12.2023 убрать костыли
         Model result = new Model();
 
         ArrayList<Vector3f> vertices = new ArrayList<>();
         ArrayList<Vector2f> textureVertices = new ArrayList<>();
         ArrayList<Vector3f> normals = new ArrayList<>();
         ArrayList<Polygon> polygons = new ArrayList<>();
-//        ArrayList<Triangle> triangulatedCopy = new ArrayList<>();
-//        TriPolyModel triPolyModel = new TriPolyModel();
 
         Collections.copy(model.vertices, vertices);
         Collections.copy(model.textureVertices, textureVertices);
         Collections.copy(model.normals, normals);
         Collections.copy(model.polygons, polygons);
-//        Collections.copy(model.triangulatedCopy.polygons, triangulatedCopy);
+        TriPolyModel triPolyModel = Triangulation.getTriangulatedModel(model);
 
-//        triPolyModel.polygons = triangulatedCopy;
         result.vertices = vertices;
         result.textureVertices = textureVertices;
         result.normals = normals;
         result.polygons = polygons;
+        result.triangulatedCopy = triPolyModel;
         result.setName(model.getName());
 
         return result;
